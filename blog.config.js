7dbe012926706be4683b88f6c9165d3ca8ebfd74
eylany <95601755+eylany@users.noreply.github.com>
2025-02-26 02:59:36 -0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: '我的博客',
   author: 'eylany',
   email: 'eylanye@gmail.com',
-  link: 'https://202323.xyz',
+  link: 'https://203023.xyz',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
